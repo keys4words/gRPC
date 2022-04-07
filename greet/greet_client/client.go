cc4299f87f4281337953290f4f847fee535e4125
keys4words <keys4words@gmail.com>
2022-04-07 08:58:02 +0300

    finished unary greet service

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -15,6 +16,23 @@ func main() {
 		log.Fatalf("could not connect: %v", err)
 	}
 	defer conn.Close()
+
 	c := greetpb.NewGreetServiceClient(conn)
-	fmt.Printf("Created client: %f", c)
+	// fmt.Printf("Created client: %f", c)
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Maxim",
+			LastName:  "Zhar",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 }
