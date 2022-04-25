7247433efeb4cb06f029f028f7f1084f8d42c4b9
keys4words <keys4words@gmail.com>
2022-04-25 15:05:29 +0300

    finished read blog rpc

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -3,11 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 
 	"github.com/keys4words/gRPC/blog/blogpb"
-	"github.com/keys4words/gRPC/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
@@ -33,45 +31,19 @@ func main() {
 		log.Fatalf("Unexpected error: %v", err)
 	}
 	fmt.Printf("Blog has been created: %v\n", res)
+	blogID := res.GetBlog().GetId()
 
-}
-
-func doUnary(c greetpb.GreetServiceClient) {
-	fmt.Println("Starting to do Unary RPC...")
-	req := &greetpb.GreetRequest{
-		Greeting: &greetpb.Greeting{
-			FirstName: "Maxim",
-			LastName:  "Zhar",
-		},
+	// read blog
+	fmt.Println("Reading the blog")
+	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "6266793a51d939d2a24d6e9a"})
+	if err2 != nil {
+		fmt.Printf("Error happened while reading: %v\n", err2)
 	}
-	res, err := c.Greet(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error while calling Greet RPC: %v", err)
-	}
-	log.Printf("Response from Greet: %v", res.Result)
-}
-
-func doSeverStreaming(c greetpb.GreetServiceClient) {
-	fmt.Println("Starting to do Server Streaming RPC...")
-	req := &greetpb.GreetManyTimesRequest{
-		Greeting: &greetpb.Greeting{
-			FirstName: "Maxim",
-			LastName:  "Zhar",
-		},
-	}
-	resStream, err := c.GreetManyTimes(context.Background(), req)
-	if err != nil {
-		log.Fatalf("Error while calling GreetManyTimes %v", err)
-	}
-	for {
-		msg, err := resStream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Error while reading stream: %v", err)
-		}
-		log.Printf("Repsonse from GreetManyTimes: %v", msg.GetResult())
+	readBlogReq := &blogpb.ReadBlogRequest{BlogId: blogID}
+	readBlogRes, readBlogErr := c.ReadBlog(context.Background(), readBlogReq)
+	if readBlogErr != nil {
+		fmt.Printf("Error happened while reading: %v\n", readBlogErr)
 	}
 
+	fmt.Printf("Blog was read: %v\n", readBlogRes)
 }
